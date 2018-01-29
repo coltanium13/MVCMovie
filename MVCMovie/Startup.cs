f265e2d482ced0967eb5c54b94982aa27beee5ce
Colton Gardner <cgardner268@gmail.com>
2018-01-28 21:44:14 -0800

    Added movie model to the app and created EF database for it.

--- MVCMovie/Startup.cs
+++ MVCMovie/Startup.cs
@@ -6,6 +6,8 @@ using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
+using Microsoft.EntityFrameworkCore;
+using MVCMovie.Models;
 
 namespace MVCMovie
 {
@@ -22,6 +24,9 @@ namespace MVCMovie
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddMvc();
+
+            services.AddDbContext<MVCMovieContext>(options =>
+                    options.UseSqlServer(Configuration.GetConnectionString("MVCMovieContext")));
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
